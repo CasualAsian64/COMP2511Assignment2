ee7fa52a790ed242bae6e5d0acce0621cc653ac7
Damon Leon <z5215434@ad.unsw.edu.au>
2021-07-10 22:24:32 +1000

    basic healing stub

--- src/unsw/loopmania/Village.java
+++ src/unsw/loopmania/Village.java
@@ -9,4 +9,25 @@ public class Village extends Building {
     public Village(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
     }    
+
+    public void healCharacter(Statistics stats) {
+        
+        // This logic should come before the healing. 
+        // if village X coordinate == character X coordinate && 
+        //    village Y coordinate == character Y coordinate
+
+        System.out.println("The characters health before healing is " + stats.getHealth());
+
+        // if heal amount is greater than 100, set health to 100 
+        if (stats.getHealth() >= 90) {
+            stats.setHealth(100);
+        }
+
+        else {
+            // Heal the character for 10 points
+            stats.setHealth(stats.getHealth() + 10);
+        }    
+
+        System.out.println("The characters health after healing is " + stats.getHealth());
+    }
 }
