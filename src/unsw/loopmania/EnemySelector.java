9f281b8b58a2ea593fd1aa379b8944bf71254e06
Jason Dam <z5165182@ad.unsw.edu.au>
2021-07-13 19:26:35 +1000

    Refactored spawn code

--- src/unsw/loopmania/EnemySelector.java
+++ src/unsw/loopmania/EnemySelector.java
@@ -2,18 +2,18 @@ package unsw.loopmania;
 
 import java.util.List;
 
+import javafx.util.Pair;
+
 public class EnemySelector {
     
-    public Enemy getEnemy(int enemySelection, PathPosition position, List<Enemy> enemies, LoopManiaWorld world) {
+    public Enemy getEnemy(int enemySelection, PathPosition position, List<Enemy> enemies, int numLoops, int vampireRespawnLoop, int zombieRespawnLoop) {
         Enemy enemy;
         boolean vampireSpawn = checkVampireSpawned(enemies);
         boolean zombieSpawn = checkZombieSpawned(enemies);
-        if (world.getNumLoops() % 5 == 0 && !vampireSpawn && world.getNumLoops() == world.getVampireRespawnLoop()) {
+        if (numLoops % 5 == 0 && !vampireSpawn && numLoops == vampireRespawnLoop) {
             enemySelection = 2;
-            world.setVampireRespawnLoop(world.getVampireRespawnLoop() + 5);
-        } else if (!zombieSpawn && world.getNumLoops() == world.getZombieRespawnLoop()) {
+        } else if (!zombieSpawn && numLoops == zombieRespawnLoop) {
             enemySelection = 1;
-            world.setZombieRespawnLoop(world.getZombieRespawnLoop() + 1);
         }
         switch (enemySelection) {
             case 0:
@@ -50,4 +50,34 @@ public class EnemySelector {
         }
         return false;
     }
+    /*
+    private Pair<Integer, Integer> possiblyGetEnemySpawnPosition() {
+        // TODO = modify this
+
+        // has a chance spawning an enemy on a tile the character isn't on or
+        // immediately before or after (currently space required = 2)...
+        Random rand = new Random();
+        int choice = rand.nextInt(2); // TODO = change based on spec... currently low value for dev purposes...
+        // TODO = change based on spec
+        // spawn 4 enemies
+        if ((choice == 0) && (enemies.size() < 4)) {
+            List<Pair<Integer, Integer>> orderedPathSpawnCandidates = new ArrayList<>();
+            int indexPosition = orderedPath.indexOf(new Pair<Integer, Integer>(character.getX(), character.getY()));
+            // inclusive start and exclusive end of range of positions not allowed
+            int startNotAllowed = (indexPosition - 2 + orderedPath.size()) % orderedPath.size();
+            int endNotAllowed = (indexPosition + 3) % orderedPath.size();
+            // note terminating condition has to be != rather than < since wrap around...
+            for (int i = endNotAllowed; i != startNotAllowed; i = (i + 1) % orderedPath.size()) {
+                orderedPathSpawnCandidates.add(orderedPath.get(i));
+            }
+
+            // choose random choice
+            Pair<Integer, Integer> spawnPosition = orderedPathSpawnCandidates
+                    .get(rand.nextInt(orderedPathSpawnCandidates.size()));
+
+            return spawnPosition;
+        }
+        return null;
+    }
+    */
 }
